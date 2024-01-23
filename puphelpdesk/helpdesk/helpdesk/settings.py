4c9127e39ba53cdc5f647a78ac2136199c93cd25
Christian Altiche <130991249+PapiChans@users.noreply.github.com>
2024-01-24 00:34:41 +0800

    v0.6.0.1

--- puphelpdesk/helpdesk/helpdesk/settings.py
+++ puphelpdesk/helpdesk/helpdesk/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,16 +83,22 @@ WSGI_APPLICATION = 'helpdesk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = { # PostgreSQL Database
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Helpdesk',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://helpdesk_tws7_user:Ttd77nFiig6a4lTtksMmnJvBdrjqSmSl@dpg-cmnuee6n7f5s73d17cig-a.singapore-postgres.render.com/helpdesk_tws7',
+        )
 }
+
+# DATABASES = { # PostgreSQL Database
+#   'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'Helpdesk',
+#        'USER': 'postgres',
+#        'PASSWORD': 'admin',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
